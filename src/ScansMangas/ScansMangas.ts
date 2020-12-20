52f1e0a7cd871cdc8c9d1fa463675768a5db6564
Boolean <danielwnewville@gmail.com>
2020-12-19 23:22:31 -0600

    Update version

--- src/ScansMangas/ScansMangas.ts
+++ src/ScansMangas/ScansMangas.ts
@@ -9,7 +9,7 @@ export class ScansMangas extends Source {
   }
 
   // @getBoolean
-  get version(): string { return '1.0.0' }
+  get version(): string { return '1.0.1' }
   get name(): string { return 'ScansMangas' }
   get icon(): string { return 'icon.png' }
   get author(): string { return 'getBoolean' }
