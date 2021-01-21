d0ad35740fedd147e7b05fec9a296aef00bad101
Boolean <danielwnewville@gmail.com>
2021-01-21 01:48:21 -0600

    Fixed getChapterDetails
    Mangakakalot updated their reader

--- src/Mangakakalot/Mangakakalot.ts
+++ src/Mangakakalot/Mangakakalot.ts
@@ -12,7 +12,7 @@ export class Mangakakalot extends Manganelo {
   }
 
   // @getBoolean
-  get version(): string { return '1.2.3'; }
+  get version(): string { return '1.2.4'; }
   get name(): string { return 'Mangakakalot' }
   get icon(): string { return 'mangakakalot.com.ico' }
   get author(): string { return 'getBoolean' }
@@ -279,14 +279,9 @@ export class Mangakakalot extends Manganelo {
   getMangakakalotChapterDetails(data: any, metadata: any): ChapterDetails {
     console.log('In getMangakakalotChapterDetails()')
     let $ = this.cheerio.load(data)
-    // let pages: string[] = []
-    let items = $('img', '.vung-doc').toArray()
+    // Mangakakalot updated their reader, '.vung-doc' is now '.container-chapter-reader'
+    let items = $('img', '.container-chapter-reader').toArray()
     let pages = Array.from(items, x=>$(x).attr('src') ?? '' )
-    /*for (let item of items) {
-      let imageUrl = $(item).attr('src') ?? ''
-      pages.push(imageUrl)
-      //console.log('Pushing image url: ' + imageUrl)
-    }*/
 
     return createChapterDetails({
       id: metadata.chapterId,
